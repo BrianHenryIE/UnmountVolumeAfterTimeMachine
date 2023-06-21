7f4148cd8e2c9293d3f6257d695e1ed42f7242ae
Brian Henry <BrianHenryIE@gmail.com>
2023-06-20 22:18:32 -0700

    `swift package init --type executable`

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "UnmountVolumeAfterTimeMachine",
+    dependencies: [
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .executableTarget(
+            name: "UnmountVolumeAfterTimeMachine",
+            dependencies: [
+            ]),
+        .testTarget(
+            name: "UnmountVolumeAfterTimeMachineTests",
+            dependencies: ["UnmountVolumeAfterTimeMachine"]),
+    ]
+)
---
+++ Sources/UnmountVolumeAfterTimeMachine/UnmountVolumeAfterTimeMachine.swift
@@ -0,0 +1,8 @@
+@main
+public struct UnmountVolumeAfterTimeMachine {
+    public private(set) var text = "Hello, World!"
+
+    public static func main() {
+        print(UnmountVolumeAfterTimeMachine().text)
+    }
+}
